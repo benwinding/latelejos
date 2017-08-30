0beb5f063cf7b647278fdc3f4250e7506fddec7a
Ben <ben.winding@gmail.com>
2017-08-31 01:18:02 +0930

    Formatting...

--- Code/UiRemote/src/RobotRemote/Services/Asynchronous/Movement/CustomNavigator.java
+++ Code/UiRemote/src/RobotRemote/Services/Asynchronous/Movement/CustomNavigator.java
@@ -7,26 +7,26 @@ import lejos.robotics.navigation.ArcRotateMoveController;
 import lejos.robotics.navigation.Pose;
 
 public class CustomNavigator implements ICustomNavigator {
-    //Configuration
-    private   double linearSpeed ; //cm per second
-    private   double mapUpdateInterval ; //seconds
-    private   double mapUpdateIntervalMs; //milliseconds
-    private   float distancePerInterval; //cm
-    private static ArcRotateMoveController pilot;
-    private static ICustomCoordinateSystem cs;
-    public static Thread moveThread = new Thread();
+  //Configuration
+  private double linearSpeed ; //cm per second
+  private double mapUpdateInterval ; //seconds
+  private double mapUpdateIntervalMs; //milliseconds
+  private float distancePerInterval; //cm
+  private static ArcRotateMoveController pilot;
+  private static ICustomCoordinateSystem cs;
+  public static Thread moveThread = new Thread();
 
   @Override
   public void Init(ICustomCoordinateSystem cs, ArcRotateMoveController pilot) {
-      if(RobotConnectionService.IsConnected()) {
-          CustomNavigator.pilot = pilot;
-          CustomNavigator.cs = cs;
+    if(RobotConnectionService.IsConnected()) {
+      CustomNavigator.pilot = pilot;
+      CustomNavigator.cs = cs;
 
-          linearSpeed = RobotConnectionService.IsConnected()? pilot.getLinearSpeed():0; //cm per second
-          mapUpdateInterval = 0.05; //seconds
-          mapUpdateIntervalMs = mapUpdateInterval * 1000; //milliseconds
-          distancePerInterval = ((float) (-linearSpeed * mapUpdateInterval)); //cm
-      }
+      linearSpeed = RobotConnectionService.IsConnected()? pilot.getLinearSpeed():0; //cm per second
+      mapUpdateInterval = 0.05; //seconds
+      mapUpdateIntervalMs = mapUpdateInterval * 1000; //milliseconds
+      distancePerInterval = ((float) (-linearSpeed * mapUpdateInterval)); //cm
+    }
   }
 
   @Override
@@ -38,42 +38,42 @@ public class CustomNavigator implements ICustomNavigator {
     }
     cs.GoingStraight(distance);
   }
-    Task<Integer> task;
+  Task<Integer> task;
 
   @Override
   public void MoveAsync(boolean ...backward) {
-        final boolean isBackward = backward.length >= 1;
-        if (isBackward) {
-            pilot.backward();
-            distancePerInterval = -1*Math.abs(distancePerInterval);
-        } else {
+    final boolean isBackward = backward.length >= 1;
+    if (isBackward) {
+      pilot.backward();
+      distancePerInterval = -1*Math.abs(distancePerInterval);
+    } else {
 
-          distancePerInterval = Math.abs(distancePerInterval);
-          pilot.forward();
+      distancePerInterval = Math.abs(distancePerInterval);
+      pilot.forward();
+    }
+    if(task==null)
+    {
+      task = new Task<Integer>() {
+        @Override
+        protected Integer call() throws Exception {
+          while (true)
+          {
+            Logger.LogCrossThread("TASK: In call loop!");
+            try {
+              Thread.sleep((long) mapUpdateIntervalMs);
+            } catch (InterruptedException interrupted) {
+              Logger.LogCrossThread("TASK: interrupted!");
+              break;
+            }
+            cs.GoingStraight(distancePerInterval); // Update coordinate system
+          }
+          return 0;
         }
-        if(task==null)
-        {
-            task = new Task<Integer>() {
-                @Override
-                protected Integer call() throws Exception {
-                    while (true)
-                    {
-                        Logger.LogCrossThread("TASK: In call loop!");
-                        try {
-                            Thread.sleep((long) mapUpdateIntervalMs);
-                        } catch (InterruptedException interrupted) {
-                            Logger.LogCrossThread("TASK: interrupted!");
-                            break;
-                        }
-                        cs.GoingStraight(distancePerInterval); // Update coordinate system
-                    }
-                    return 0;
-                }
-            };
-            moveThread = new Thread(task);
-            moveThread.setDaemon(true);
-            moveThread.start();
-        }
+      };
+      moveThread = new Thread(task);
+      moveThread.setDaemon(true);
+      moveThread.start();
+    }
   }
 
   @Override
@@ -88,11 +88,11 @@ public class CustomNavigator implements ICustomNavigator {
   @Override
   public void Stop() {
     try{
-        pilot.stop();
-        task.cancel();
-        task =null;
+      pilot.stop();
+      task.cancel();
+      task =null;
     } catch (Exception ignored) {
-        Logger.LogCrossThread("Exception: Unable to stop! Reason:"+ ignored.getMessage());
+      Logger.LogCrossThread("Exception: Unable to stop! Reason:"+ ignored.getMessage());
     }
   }
 
