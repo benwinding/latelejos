93a989f98037d574914395bc4cb37b4a7531a3ff
Sean Hennessy <sean.hennessy@student.adelaide.edu.au>
2017-09-14 16:59:01 +0930

    Code cleanup
    Removed some unnecessary whitespace

--- Code/UiRemote/src/RobotRemote/Services/MapHandlers/IRobotMapTranslator.java
+++ Code/UiRemote/src/RobotRemote/Services/MapHandlers/IRobotMapTranslator.java
@@ -5,6 +5,6 @@ package RobotRemote.Services.MapHandlers;
 import javax.xml.bind.JAXBException;
 
 public interface IRobotMapTranslator {
-  public String createXml(Lunarovermap mapTransferObject);
+  public String createXml(Lunarovermap mapTransferObject) throws JAXBException;
   public Lunarovermap createMapObject(String mapXml) throws JAXBException;
 }
--- Code/UiRemote/src/RobotRemote/Services/MapHandlers/RobotMapTranslator.java
+++ Code/UiRemote/src/RobotRemote/Services/MapHandlers/RobotMapTranslator.java
@@ -4,46 +4,25 @@ package RobotRemote.Services.MapHandlers;
 import javax.xml.bind.*;
 import java.io.File;
 
-/*<!DOCTYPE lunarrovermap [  <!-- A Luna Rover map contianing one boundary, vehicle status, apollo landing site,
-rover landing site and track to color mapping, with any number of obstacles, zones, tracks and additional attributes.
- The Luna Rover map uses a global unit of mesurement for all measurmenets -->
-
-*/
-
-
 public class RobotMapTranslator implements IRobotMapTranslator {
     public RobotMapTranslator(){
 
         return;
     }
-
-
-    public String createXml(Lunarovermap mapTransferObject){
-
-        JAXBContext jaxbContext = null;
-        try {
-            jaxbContext = JAXBContext.newInstance("RobotRemote.Services.MapHandlers");
-            Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
-            jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT,true);
-            
-                jaxbMarshaller.marshal(mapTransferObject,System.out);
-
-        } catch (JAXBException e) {
-            e.printStackTrace();
-        }
-
+    public String createXml(Lunarovermap mapTransferObject)throws JAXBException{
+        JAXBContext jaxbContext = JAXBContext.newInstance("RobotRemote.Services.MapHandlers");
+        Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+        jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT,true);
+        jaxbMarshaller.marshal(mapTransferObject,System.out);
         return " ";
     }
 
 
     public Lunarovermap createMapObject(String mapXml)throws JAXBException{
-
-    JAXBContext jaxbContext = JAXBContext.newInstance("RobotRemote.Services.MapHandlers");
-
-    Unmarshaller unmarhsaller = jaxbContext.createUnmarshaller();
-
+        JAXBContext jaxbContext = JAXBContext.newInstance("RobotRemote.Services.MapHandlers");
+        Unmarshaller unmarhsaller = jaxbContext.createUnmarshaller();
         Lunarovermap lunarmap = (Lunarovermap) unmarhsaller.unmarshal(new File(mapXml));
-    return lunarmap;
+        return lunarmap;
     }
 
 }
