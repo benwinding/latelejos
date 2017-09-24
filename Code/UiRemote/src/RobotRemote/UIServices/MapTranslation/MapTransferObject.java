dc265ceb392e032344c90ca6831977f55bb1bfe6
Unknown <s.p.hennessy7@gmail.com>
2017-09-24 18:58:00 +0930

    Auto stash before merge of "master" and "origin/master"

--- Code/UiRemote/src/RobotRemote/UIServices/MapTranslation/MapTransferObject.java
+++ Code/UiRemote/src/RobotRemote/UIServices/MapTranslation/MapTransferObject.java
@@ -23,4 +23,55 @@ public class MapTransferObject {
         this.boundary=boundary;
         this.roverLandingSite=roverLandingSite;
     }
+
+    public void setCurrentPosition(MapPoint currentPosition) {
+        this.currentPosition = currentPosition;
+    }
+    public void setNoGoZones(ArrayList<MapPoint> noGoZones) {
+        this.noGoZones = noGoZones;
+    }
+    public void setBoundary(ArrayList<MapPoint> boundary) {
+        this.boundary = boundary;
+    }
+    public void setCraters(ArrayList<MapPoint> craters) {
+        this.craters = craters;
+    }
+    public void setRadiation(ArrayList<MapPoint> radiation) {
+        this.radiation = radiation;
+    }
+    public void setRoverLandingSite(MapPoint roverLandingSite) {
+        this.roverLandingSite = roverLandingSite;
+    }
+    public void setTracks(ArrayList<MapPoint> tracks) {
+        this.tracks = tracks;
+    }
+
+    public ArrayList<MapPoint> getNoGoZones() {
+        return noGoZones;
+    }
+
+    public ArrayList<MapPoint> getRadiation() {
+        return radiation;
+    }
+
+    public MapPoint getRoverLandingSite() {
+        return roverLandingSite;
+    }
+
+    public MapPoint getCurrentPosition() {
+        return currentPosition;
+    }
+
+    public ArrayList<MapPoint> getCraters() {
+        return craters;
+    }
+
+    public ArrayList<MapPoint> getTracks() {
+        return tracks;
+    }
+
+    public ArrayList<MapPoint> getBoundary() {
+        return boundary;
+    }
+
 }
--- Code/UiRemote/src/RobotRemote/UIServices/MapTranslation/MapTranslator.java
+++ Code/UiRemote/src/RobotRemote/UIServices/MapTranslation/MapTranslator.java
@@ -1,5 +1,6 @@
 package RobotRemote.UIServices.MapTranslation;
 
+import RobotRemote.Models.MapPoint;
 import RobotRemote.RobotServices.Movement.LocationState;
 import RobotRemote.UIServices.MapTranslation.XmlTranslation.Lunarovermap;
 import RobotRemote.UIServices.MapTranslation.XmlTranslation.XmlTranslator;
@@ -7,17 +8,24 @@ import RobotRemote.UIServices.MapTranslation.XmlTranslation.XmlTranslator;
 import javax.xml.bind.JAXBException;
 
 public class MapTranslator implements IMapTranslator{
+  //input: xmlstring, outputs mapTransferObject
   @Override
   public MapTransferObject GetMapFromXmlString(String xmlString) throws JAXBException {
+    //create lunarRoverMap object:
     Lunarovermap mapFromString = new XmlTranslator().createMapObject(xmlString);
-    LocationState newLocation = new LocationState(0,0,0);
+    //Convert LunarRoverMap to maptransferObject:
+
+    //test current point:
+    MapPoint current =new MapPoint(666,666,10);
     MapTransferObject mapTransferObject = null;//new MapTransferObject();
     // .....
     // Set all mapTransfer properties from lunarObject
     // .....
+    mapTransferObject.setCurrentPosition(current);
     return mapTransferObject;
   }
 
+  //inputs mapTransferObject, outputs xmlstring
   @Override
   public String GetXmlStringFromMap(MapTransferObject mapTransferObject) throws JAXBException {
     Lunarovermap lunarovermap = new Lunarovermap();
