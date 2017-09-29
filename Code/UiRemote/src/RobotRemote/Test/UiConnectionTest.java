579de68abd0222f892bf48d5858632a0273af16b
issac725 <issac725725@gmail.com>
2017-09-29 16:45:35 +0930

    Change about pop up window

--- Code/UiRemote/src/RobotRemote/Test/UiConnectionTest.java
+++ Code/UiRemote/src/RobotRemote/Test/UiConnectionTest.java
@@ -1,13 +1,29 @@
 package RobotRemote.Test;
 
+import RobotRemote.RobotServices.Connection.RobotConnectionService;
+import RobotRemote.RobotServices.Movement.IMovementService;
+import RobotRemote.RobotServices.Movement.MovementService;
+import RobotRemote.RobotServices.Sensors.SensorsService;
+import RobotRemote.RobotStateMachine.StateMachineBuilder;
+import RobotRemote.Shared.*;
+import RobotRemote.UI.Views.RootController;
+import RobotRemote.UIServices.MapHandlers.MapInputEventHandlers;
+import RobotRemote.UIServices.UiUpdater.UiUpdaterService;
+import com.google.common.eventbus.EventBus;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.IOException;
+
+import static javafx.application.Application.launch;
+
 public class UiConnectionTest {
     @Test
-    public void Timeout(){
+    public void Timeout() throws IOException {
 
     }
-
-
 }
