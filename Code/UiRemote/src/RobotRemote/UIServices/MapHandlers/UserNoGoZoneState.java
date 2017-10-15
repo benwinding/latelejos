57af144328350d4b4e15c3eb073820af08bc46ad
Ben <ben.winding@gmail.com>
2017-10-15 23:29:54 +1030

    Methods to detect if robot is within a ngz shape

--- Code/UiRemote/src/RobotRemote/UIServices/MapHandlers/UserNoGoZoneState.java
+++ Code/UiRemote/src/RobotRemote/UIServices/MapHandlers/UserNoGoZoneState.java
@@ -1,10 +1,14 @@
 package RobotRemote.UIServices.MapHandlers;
 
 import RobotRemote.Models.MapPoint;
+import RobotRemote.Shared.RobotConfiguration;
 import lejos.utility.Matrix;
 
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class UserNoGoZoneState {
   private List<List<MapPoint>> allNgzSets;
@@ -45,6 +49,37 @@ public class UserNoGoZoneState {
   void FinishedNgzSet() {
     if(this.allNgzSets.size() > 0 && this.getCurrentNgzPoints() != null)
       this.CopyFirstPointToLastPosition(this.getCurrentNgzPoints());
+  }
+
+  public boolean isPointInNgz(MapPoint point) {
+    Point testPoint = new Point((int)point.x,(int)point.y);
+    for(List<MapPoint> ngzSet: this.allNgzSets) {
+      Polygon polygon = new Polygon();
+      for(MapPoint mapPoint: ngzSet) {
+        polygon.addPoint((int)mapPoint.x,(int)mapPoint.y);
+      }
+      if(polygon.contains(testPoint))
+        return true;
+    }
+    return false;
+  }
+
+  public boolean isRobotInNgz(MapPoint robotLocation, RobotConfiguration config) {
+    int robotLong = config.robotPhysicalLength;
+    int x = (int)robotLocation.x - robotLong / 2;
+    int y = (int)robotLocation.y - robotLong / 2;
+    Rectangle2D testPoint = new Rectangle(x,y,robotLong,robotLong);
+    for(List<MapPoint> ngzSet: this.allNgzSets) {
+      Polygon polygon = new Polygon();
+      for(MapPoint mapPoint: ngzSet) {
+        polygon.addPoint((int)mapPoint.x,(int)mapPoint.y);
+      }
+      if(polygon.intersects(testPoint))
+        return true;
+      if(polygon.contains(testPoint))
+        return true;
+    }
+    return false;
   }
 }
 
